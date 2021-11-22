225804221e49f71efc82e418aacdada315cbf5ae
박수정 <zxzx4342@naver.com>
2021-11-22 22:11:35 +0900

    레트로핏 통신 객체 + 기능목록 인터페이스 결합  - BaseActivity의 멤버변수로 담아두자.  - 모든 화면이 상속 받게됨.  - 화면 입장에선 apiService.서버호출() 활용

--- app/src/main/java/com/crystal/keppthetime_20211122/BaseActivity.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/BaseActivity.kt
@@ -3,15 +3,28 @@ package com.crystal.keppthetime_20211122
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.crystal.keppthetime_20211122.api.ServerAPI
+import com.crystal.keppthetime_20211122.api.ServerAPIService
+import retrofit2.Retrofit
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext : Context
 
+//    모든 화면에서 서버통신이 필요할것 같다.
+//    모든 화면에 레트로핏 / API 기능 목록을 결합한 변수를 미리 만들어서 물려주자.
+    private lateinit var retrofit: Retrofit
+    lateinit var apiService : ServerAPIService
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+//        서버통신 관련 세팅
+        retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
 
     }
 
