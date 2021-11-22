60d4f64d111f60cca9fb749f5bfb32bbc0d5e004
박수정 <zxzx4342@naver.com>
2021-11-22 21:38:34 +0900

    로그인 화면 UI 추가

--- app/src/main/java/com/crystal/keppthetime_20211122/MainActivity.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
         setupEvents()
         setValues()
 
