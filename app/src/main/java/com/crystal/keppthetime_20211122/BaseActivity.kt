b8bf68b293192685e9d409d448099833b4730fe8
박수정 <zxzx4342@naver.com>
2021-11-22 20:55:02 +0900

    Initial commit

---
+++ app/src/main/java/com/crystal/keppthetime_20211122/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.crystal.keppthetime_20211122
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    lateinit var mContext : Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mContext = this
+
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
---
+++ app/src/main/java/com/crystal/keppthetime_20211122/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.crystal.keppthetime_20211122
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.crystal.keppthetime_20211122.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
