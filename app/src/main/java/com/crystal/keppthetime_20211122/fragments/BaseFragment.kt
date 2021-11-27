bf2f5e75b12a653da0e147d324b3c231553b196d
박수정 <zxzx4342@naver.com>
2021-11-28 01:00:02 +0900

    BaseFragment : 모든 프래그먼트가 공통적으로 갖는 기능 / 변수 모아두는 클래스

---
+++ app/src/main/java/com/crystal/keppthetime_20211122/fragments/BaseFragment.kt
@@ -0,0 +1,20 @@
+package com.crystal.keppthetime_20211122.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    lateinit var mContext : Context
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mContext = requireContext()
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+}
