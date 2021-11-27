2af475e24d41f7a6d8a6bbeb531c8a4158dcc873
박수정 <zxzx4342@naver.com>
2021-11-27 18:49:15 +0900

    회원가입 화면 추가

---
+++ app/src/main/java/com/crystal/keppthetime_20211122/SignUpActivity.kt
@@ -0,0 +1,26 @@
+package com.crystal.keppthetime_20211122
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.crystal.keppthetime_20211122.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity() {
+
+    lateinit var binding : ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
