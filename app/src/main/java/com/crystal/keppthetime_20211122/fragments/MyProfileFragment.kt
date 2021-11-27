2924ff1dab1ae88e76ea63557c04938d9f1612ce
박수정 <zxzx4342@naver.com>
2021-11-28 01:17:50 +0900

    프래그먼트의 데이터 바인딩

--- app/src/main/java/com/crystal/keppthetime_20211122/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/fragments/MyProfileFragment.kt
@@ -1,13 +1,23 @@
 package com.crystal.keppthetime_20211122.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.crystal.keppthetime_20211122.R
+import com.crystal.keppthetime_20211122.databinding.FragmentMyProfileBinding
+import com.crystal.keppthetime_20211122.datas.BasicResponse
+import com.crystal.keppthetime_20211122.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfileFragment : BaseFragment() {
+
+    lateinit var binding : FragmentMyProfileBinding
 
 
     override fun onCreateView(
@@ -16,8 +26,9 @@ class MyProfileFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_profile, container, false)
 
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -35,10 +46,35 @@ class MyProfileFragment : BaseFragment() {
 
 //        내 정보를 서버에서 받아오자. -> 이미지 반영 / 닉네임 반영
 
+        getMyInfoFromServer()
 //        1. 프래그먼트에서 retrofit 어떻게 활용?
 
 //        2. dataBinding -> 프래그먼트에서는 어떻게 데이터바인딩?
 
+    }
+
+    fun getMyInfoFromServer() {
+        apiService.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object  : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful ) {
+                    val br = response.body()!!
+
+//                    닉네임 / 프로필 이미지 주소 추출 => UI 반영?
+
+
+
+
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+            }
+
+
+        })
     }
 
 }
