f32f07127fe4a96146c9f4d085e830a8a82c208a
박수정 <zxzx4342@naver.com>
2021-11-27 19:20:04 +0900

    메인화면 이름변경 -> LoginActivity

--- app/src/main/java/com/crystal/keppthetime_20211122/LoginActivity.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/LoginActivity.kt
@@ -1,24 +1,23 @@
 package com.crystal.keppthetime_20211122
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.crystal.keppthetime_20211122.databinding.ActivityMainBinding
+import com.crystal.keppthetime_20211122.databinding.ActivityLoginBinding
 import com.crystal.keppthetime_20211122.datas.BasicResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
 
