82fd90a1f1a9c8197a5390a77562a10e4f960358
박수정 <zxzx4342@naver.com>
2021-11-27 18:43:21 +0900

    실제 로그인 기능 수행  - 응답 : BasicResponse 형태로 돌아옴.  - body를 변수에 담아서 활용 -? 하위 변수에 파싱까지 전부 되어있었다.

--- app/src/main/java/com/crystal/keppthetime_20211122/MainActivity.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/MainActivity.kt
@@ -2,8 +2,14 @@ package com.crystal.keppthetime_20211122
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.crystal.keppthetime_20211122.databinding.ActivityMainBinding
+import com.crystal.keppthetime_20211122.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
@@ -26,7 +32,31 @@ class MainActivity : BaseActivity() {
             val inputPassword = binding.edtPassword.text.toString()
 
 
-//            2. 서버에 로그인 API 호출 -> Retrofit
+//            2. 서버에 로그인 API 호출 -> Retrofit Callback은 retrofit2를 선택
+
+            apiService.postRequestLogin(inputEmail, inputPassword).enqueue( object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+//                    최종 성공 / 실패 여부에 따라 별도 코딩
+                    if (response.isSuccessful) {
+
+                        val basicResponse = response.body()!!
+                        Log.d("로그인성공", basicResponse.message)
+
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                    Toast.makeText(mContext, "서버 연결에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+
+            } )
 
 
         }
