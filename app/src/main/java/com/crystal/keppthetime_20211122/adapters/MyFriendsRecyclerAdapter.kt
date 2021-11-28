2bfb072b3cfefb643897f9b40a084e0925304058
박수정 <zxzx4342@naver.com>
2021-11-28 12:36:32 +0900

    리싸이클러 어댑터 기초작업

---
+++ app/src/main/java/com/crystal/keppthetime_20211122/adapters/MyFriendsRecyclerAdapter.kt
@@ -0,0 +1,34 @@
+package com.crystal.keppthetime_20211122.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.crystal.keppthetime_20211122.R
+import com.crystal.keppthetime_20211122.datas.UserData
+
+class MyFriendsRecyclerAdapter(val mContext: Context, val mList: ArrayList<UserData>) : RecyclerView.Adapter<MyFriendsRecyclerAdapter.MyFriendViewHolder>() {
+
+    inner class MyFriendViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyFriendViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.my_friend_list, parent, false)
+
+        return MyFriendViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyFriendViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
+
+
+}
