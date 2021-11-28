10ad87c959cfc06fb788050547faf8265abc6c39
박수정 <zxzx4342@naver.com>
2021-11-28 13:44:57 +0900

    스플래쉬 화면 추가 -> 시작화면으로 변경

---
+++ app/src/main/java/com/crystal/keppthetime_20211122/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.crystal.keppthetime_20211122
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
