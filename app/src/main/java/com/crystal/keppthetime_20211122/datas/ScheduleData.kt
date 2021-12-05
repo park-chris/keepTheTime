e95674903d8ea44273cf98d007503fe3a570daf3
박수정 <zxzx4342@naver.com>
2021-12-05 14:34:47 +0900

    스케쥴 목록 어댑터 기초작업

---
+++ app/src/main/java/com/crystal/keppthetime_20211122/datas/ScheduleData.kt
@@ -0,0 +1,15 @@
+package com.crystal.keppthetime_20211122.datas
+
+
+
+class ScheduleData(
+
+    var id: Int,
+    var title: String,
+    var dateTime: String,
+    var place: String,
+    var latitude: Double,
+    var longitude: Double,
+    var created_at: String,
+) {
+}
