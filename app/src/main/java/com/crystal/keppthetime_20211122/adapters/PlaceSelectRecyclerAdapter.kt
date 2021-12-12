974307bbe45d98c219039db7ab29322201ffe856
박수정 <zxzx4342@naver.com>
2021-12-12 14:58:41 +0900

    얼럿 내부에서 사용할 리싸이클러 어댑터 생성중

---
+++ app/src/main/java/com/crystal/keppthetime_20211122/adapters/PlaceSelectRecyclerAdapter.kt
@@ -0,0 +1,19 @@
+package com.crystal.keppthetime_20211122.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PlaceSelectRecyclerAdapter : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
+
+    inner class PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
+    }
+
+    override fun onBindViewHolder(holder: ???, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+}
