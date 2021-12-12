ae933ca1cde4ff632650a0e88dbf97888333e78a
박수정 <zxzx4342@naver.com>
2021-12-12 15:01:49 +0900

    리싸잌클러 어댑터 뼈대작업 마무리

--- app/src/main/java/com/crystal/keppthetime_20211122/adapters/PlaceSelectRecyclerAdapter.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/adapters/PlaceSelectRecyclerAdapter.kt
@@ -1,19 +1,31 @@
 package com.crystal.keppthetime_20211122.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.crystal.keppthetime_20211122.R
+import com.crystal.keppthetime_20211122.datas.PlaceData
 
-class PlaceSelectRecyclerAdapter : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
+class PlaceSelectRecyclerAdapter(val mContext: Context, val mList: List<PlaceData>) : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
 
     inner class PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.place_list_item, parent)
+        return  PlaceViewHolder(row)
+
     }
 
-    override fun onBindViewHolder(holder: ???, position: Int) {
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+
+
     }
 
-    override fun getItemCount(): Int {
-    }
+    override fun getItemCount() = mList.size
+
+
 }
