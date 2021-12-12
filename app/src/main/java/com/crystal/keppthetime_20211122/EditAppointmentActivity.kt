5abeb0c5d539088b63971f35e074ae13ff4f80cb
박수정 <zxzx4342@naver.com>
2021-12-12 15:55:03 +0900

    별도 함수 추가 - 임시로 장소명을 반영

--- app/src/main/java/com/crystal/keppthetime_20211122/EditAppointmentActivity.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/EditAppointmentActivity.kt
@@ -158,7 +158,16 @@ class EditAppointmentActivity : BaseActivity() {
                         placeAdapter.onItemClickListener = object : PlaceSelectRecyclerAdapter.OnItemClickListener {
                             override fun onItemClick(data: PlaceData) {
 
-                                Toast.makeText(mContext, data.placeName, Toast.LENGTH_SHORT).show()
+//                                임시 기능 : 토스트로 가게 이름 출력
+//                                Toast.makeText(mContext, data.placeName, Toast.LENGTH_SHORT).show()
+
+//                                실제 기능 : 장소를 선택한 곳으로 지정하기.
+
+                                setPlaceDataToNaverMap(data)
+
+//                                추가 기능 : 열려있는 팝업창 닫기
+
+
 
                             }
 
@@ -536,4 +545,15 @@ class EditAppointmentActivity : BaseActivity() {
         super.onLowMemory()
         binding.naverMapView.onLowMemory()
     }
+
+//    장소를 선택하면 => 지도에 반영해주는 함수
+
+    fun setPlaceDataToNaverMap(placeData: PlaceData) {
+
+//        약속장소 입력값 변경
+
+        binding.edtPlace.setText( placeData.placeName )
+
+    }
+
 }
