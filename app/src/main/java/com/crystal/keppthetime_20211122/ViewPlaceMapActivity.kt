6e74003cc36969cdc9f16ccd54eedb562e0689a8
박수정 <zxzx4342@naver.com>
2021-12-12 12:39:23 +0900

    지도 상세보기 화면 -> 오디세이 라이브러리 이용  - 출발지 : 임시 집주소

--- app/src/main/java/com/crystal/keppthetime_20211122/ViewPlaceMapActivity.kt
+++ app/src/main/java/com/crystal/keppthetime_20211122/ViewPlaceMapActivity.kt
@@ -9,6 +9,10 @@ import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
+import com.odsay.odsayandroidsdk.API
+import com.odsay.odsayandroidsdk.ODsayData
+import com.odsay.odsayandroidsdk.ODsayService
+import com.odsay.odsayandroidsdk.OnResultCallbackListener
 
 class ViewPlaceMapActivity : BaseActivity() {
 
@@ -92,6 +96,43 @@ class ViewPlaceMapActivity : BaseActivity() {
 
                 return@setOnClickListener true
             }
+
+//            ODsay 라이브러리 재활용 -> JSON 파싱 => 출발지 ~ 도착지 대중교통 경로 지도에 표시.
+//            복잡한 모양의 JSONObject 직접 파싱 복습 => ~ 12:30분
+
+//            출발지 정보 ~ 도착지 API 호출
+//            or 본인 집 좌표 직접 출발지 지정
+
+            val startingPoint = LatLng( 37.54773339330566, 127.04605316673303 )
+
+//            오디세이 라이브러리로 -> 대중교통 경로 API 호출
+
+            val myODsayService = ODsayService.init(mContext, resources.getString(R.string.odsay_key))
+
+            myODsayService.requestSearchPubTransPath(
+                startingPoint.latitude.toString(),
+                startingPoint.longitude.toString(),
+                mScheduleData.longitude.toString(),
+                mScheduleData.latitude.toString(),
+                null,
+                null,
+                null,
+                object : OnResultCallbackListener {
+                    override fun onSuccess(p0: ODsayData?, p1: API?) {
+                    }
+
+                    override fun onError(p0: Int, p1: String?, p2: API?) {
+                    }
+
+                }
+
+
+            )
+
+
+            val endPoint = LatLng(mScheduleData.latitude, mScheduleData.longitude)
+
+
 
 
         }
